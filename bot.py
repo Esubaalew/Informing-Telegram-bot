74fc80c448232f12f66f5197046c1af54b39a299
Esubalew Chekol <esubalewchekol6@gmail.com>
2024-04-06 11:52:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 from telegram.ext.updater import Updater #upm package(python-telegram-bot)
 
-from telegram import Update #upm package(python-telegram-bot)
+from telegram import Update, ChatMember #upm package(python-telegram-bot),
 from telegram.ext.callbackcontext import CallbackContext
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.messagehandler import MessageHandler
-from replit import db
 from telegram.ext.filters import Filters
 import os
+from telegram.error import BadRequest
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+CHANNEL_USERNAME = "@esubalewbots"
 
 def start(update: Update, context: CallbackContext):
   name=update.message.from_user.first_name
@@ -16,7 +19,7 @@ def start(update: Update, context: CallbackContext):
     )
   update.message.reply_text(
        " I will give you any information about the user. To do that you only forward a text message from the user you need info about.\n\
-    or use the following commands: "+ 
+    or use the following commands: "+
 """
 /start - starts the bot
 /myinfo - possible informarion about your account
@@ -34,7 +37,7 @@ def help(update: Update, context: CallbackContext):
 /myinfo - possible informarion about your account
 /help - for all commands list
 /about - to know about bot designer
-    
+
   """)
 def about(update: Update, context: CallbackContext):
     update.message.reply_text("""
@@ -51,104 +54,138 @@ def others(update: Update, context: CallbackContext):
     @AAU_Robot
 
 """)
-def unknown(update: Update, context: CallbackContext): 
+def send_join_channel_button(chat_id, context):
+  button = InlineKeyboardButton("Join Channel",
+                                url="https://t.me/esubalewbots")
+  keyboard = [[button]]
+  reply_markup = InlineKeyboardMarkup(keyboard)
+  context.bot.send_message(chat_id=chat_id,
+                           text="To use this bot, please join our channel:",
+                           reply_markup=reply_markup)
 
-    update.message.reply_text( 
+def check_user_status(update: Update, context: CallbackContext) -> str:
+  user_id = update.effective_user.id
+  try:
+    chat_member = context.bot.get_chat_member(chat_id=CHANNEL_USERNAME,
+                                              user_id=user_id)
+    user_status = chat_member.status
 
-        "Sorry '%s' is not a valid command" % update.message.text) 
+    return user_status
 
-def getInfo(update: Update, context: CallbackContext):
-    try:
-        userID=str(update.message.forward_from.id)
-        userFirst=str(update.message.forward_from.first_name)
-        userLast=str(update.message.forward_from.last_name)
-        userFull=str(update.message.forward_from.last_name)
-        userName=str(update.message.forward_from.username)
-        
-  
-        idd="The user's telegram ID is "+ userID
-        Fname="The user's telegram First Name is "+ userFirst 
-        Lname="The user's telegram Last Name is "+ userLast
-        FuName="The user's telegram Full Name is "+ userFull
-        userN="The user's telegram username is "+ userName
-        see=False
-        if userLast=='None':
-            see=True
-            if see is True:
-                FuName="The user's Full Name is "+userFirst
-                Lname="The user did not set last name to Telegram"
-        if userFirst=='None':
-            see=True
-            if see is True:
-                Fname="The user did not set first name to Telegram"
-    
+  except BadRequest as e:
+    print(f"Error: {e}")
+    return "error"
 
-        if userName=='None':
-            see=True
-            if see is True:
-                userN="The user did not set username to Telegram"
-
-
-        update.message.reply_text(
-        'ID: ' +idd+'\n'+ 'First Name: '+ Fname+ '\n'+ 'Last Name: '+
-        Lname +'\n' + 'Full Name: '+ FuName + '\n'+ 'Username: ' +userN
-          )
-    except:
-        username=update.message.forward_sender_name
-        name=update.message.from_user.first_name
-        sorry='Dear '+ name+ ', This user restricted his account privacy. So some of the informations are not accesible'
-
-        update.message.reply_text(
-            sorry+ " \n"+ 'The only inormation: \n Name of User: '+ username
-
-        )
-def myinfo(update: Update, context: CallbackContext):
-    userID=update.message.from_user.id
-    userFirst=update.message.from_user.first_name
-    userLast=update.message.from_user.last_name
-    userFull=update.message.from_user.full_name
-    userName=update.message.from_user.username
-    userID=str(userID)
-    userFirst=str(userFirst)
-    userLast=str(userLast)
-    userFull=str(userFull)
-    userName=str(userName)
-    idd='Your telegram id is '+ userID
-    Fname='Your First name is '+ userFirst 
-    Lname='Your Last name is '+ userLast
-    FuName='Your Full  name is '+ userFull
-    userN='Your Telegram username is '+ userName
-    
-    see=False
-    if userLast=='None':
-        see=True
-        if see is True:
-            FuName="Your  Full Name is "+userFirst
-            Lname="You did not set your last name to Telegram"
-            
-    if userFirst=='None':
-        see=True
-        if see is True:
-            Fname="You did not set your first name to Telegram"
-    
-
-    if userName=='None':
-        see=True
-        if see is True:
-            userN="You did not set your username to Telegram"
-    
-
-
+def unknown(update: Update, context: CallbackContext):
 
     update.message.reply_text(
-       'ID: ' +idd+'\n'+ 'First Name: '+ Fname+ '\n'+ 'Last Name: '+
-        Lname +'\n' + 'Full Name: '+ FuName + '\n'+ 'Username: ' +userN
 
+        "Sorry '%s' is not a valid command" % update.message.text)
+
+import json
+
+def getInfo(update: Update, context: CallbackContext):
+    user_status = check_user_status(update, context)
+    if user_status == "error":
+        update.message.reply_text("An error occurred. Please try again later.")
+        return
+    if user_status not in [
+      ChatMember.MEMBER, ChatMember.ADMINISTRATOR, ChatMember.CREATOR
+  ]:
+      update.message.reply_text(
+      f"Your current status: {user_status}\nPlease join {CHANNEL_USERNAME} before using the bot."
     )
+      send_join_channel_button(update.message.chat_id, context)
+      return
+    try:
+        userID = str(update.message.forward_from.id)
+        userFirst = str(update.message.forward_from.first_name)
+        userLast = str(update.message.forward_from.last_name)
+        userFull = str(update.message.forward_from.last_name)
+        userName = str(update.message.forward_from.username)
+
+        response = {
+            "ID": userID,
+            "First Name": userFirst,
+            "Last Name": userLast,
+            "Full Name": userFull,
+            "Username": userName
+        }
+
+        update.message.reply_text(json.dumps(response, indent=4))
+    except Exception as e:
+        username = update.message.forward_sender_name
+        name = update.message.from_user.first_name
+        sorry = 'Dear ' + name + ', This user restricted his account privacy. So some of the informations are not accessible'
+
+        response = {
+            "Message": sorry,
+            "Name": username
+        }
+
+        update.message.reply_text(json.dumps(response, indent=4))
+
+def myinfo(update: Update, context: CallbackContext):
+    user_status = check_user_status(update, context)
+    if user_status == "error":
+        update.message.reply_text("An error occurred. Please try again later.")
+        return
+    if user_status not in [
+      ChatMember.MEMBER, ChatMember.ADMINISTRATOR, ChatMember.CREATOR
+  ]:
+      update.message.reply_text(
+      f"Your current status: {user_status}\nPlease join {CHANNEL_USERNAME} before using the bot."
+    )
+      send_join_channel_button(update.message.chat_id, context)
+      return
+    userID = str(update.message.from_user.id)
+    userFirst = str(update.message.from_user.first_name)
+    userLast = str(update.message.from_user.last_name)
+    userFull = str(update.message.from_user.full_name)
+    userName = str(update.message.from_user.username)
+
+    response = {
+        "ID": userID,
+        "First Name": userFirst,
+        "Last Name": userLast,
+        "Full Name": userFull,
+        "Username": userName
+    }
+
+    update.message.reply_text(json.dumps(response, indent=4))
+
+def myinfo(update: Update, context: CallbackContext):
+    user_status = check_user_status(update, context)
+    if user_status == "error":
+        update.message.reply_text("An error occurred. Please try again later.")
+        return
+    if user_status not in [ChatMember.MEMBER, ChatMember.ADMINISTRATOR, ChatMember.CREATOR]:
+        update.message.reply_text(
+            f"Your current status: {user_status}\nPlease join {CHANNEL_USERNAME} before using the bot."
+        )
+        send_join_channel_button(update.message.chat_id, context)
+        return
+
+    userID = str(update.message.from_user.id)
+    userFirst = str(update.message.from_user.first_name)
+    userLast = str(update.message.from_user.last_name)
+    userFull = str(update.message.from_user.full_name)
+    userName = str(update.message.from_user.username)
+
+    response = {
+        "ID": userID,
+        "First Name": userFirst,
+        "Last Name": userLast,
+        "Full Name": userFull,
+        "Username": userName
+    }
+
+    update.message.reply_text(json.dumps(response, indent=4))
+
 
 def main():
-  updater = Updater(os.getenv("TOKEN"))
-    
+  updater = Updater("TOKEN")
+
   #updater = Updater(TOKEN, use_context=True)
   disp = updater.dispatcher
   #some=update.message.text
@@ -161,12 +198,12 @@ def main():
     Filters.all & (~Filters.command), getInfo))
   disp.add_handler(MessageHandler(Filters.command, unknown))
   #disp.add_handler(MessageHandler(Filters.some, myinfo))
-  
-  
+
+
   updater.start_polling()
-  app.run(host='0.0.0.0', port=8080)
+#   app.run(host='0.0.0.0', port=8080)
 #updater.start_polling()
-                          
+
 #updater.idle()
 #updater.idle()
 #app.run(host='0.0.0.0', port=8080)
